a549e7881ff1cafe42ff334deb4f28e770c40a6f
yousouphe <yousouphe@gmail.com>
2024-04-09 11:50:00 +0100

    Adding updated  codes and the PCA script

--- Data Quality Checks and Plots/PCA.R
+++ Data Quality Checks and Plots/PCA.R
@@ -11,13 +11,29 @@ df1<-read_csv('/Users/user/Downloads/all_malaria_data.csv')
 df1 <- df1 %>%
   mutate_if(is.character, as.factor)
 
+
+
+
+theme_manuscript <- function(){
+  theme_bw() +
+    theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+          plot.title = element_text(hjust = 0.5),
+          axis.text.x = element_text(size = 10, color = "#334425"),
+          axis.text.y = element_text( size = 10, color = "#334425"),
+          #axis.title.x = element_blank(),
+          axis.title.y = element_text(size =8),
+          legend.title=element_text(size=8, colour = '#334425'),
+          legend.text =element_text(size = 8, colour = '#334425'),
+          legend.key.height = unit(1, "cm"))
+}
+
 #Replace those not sharing compounds with 1 
 df1$number_hh_sharing_compound <- ifelse(is.na(df1$number_hh_sharing_compound.x), 1, df1$number_hh_sharing_compound.x)
 
-unique_water_sources <- unique(df1$drinking_water_source.x)
-unique_water_sources_tasks <- unique(df1$basic_hh_tasks_water_source.x)
+#unique_water_sources <- unique(df1$drinking_water_source.x)
+#unique_water_sources_tasks <- unique(df1$basic_hh_tasks_water_source.x)
 unique_main_energy_sources <- unique(df1$basic_hh_tasks_energy_source.x)
-unique_toilet_facilities <- unique(df1$toilet_facility.x)
+#unique_toilet_facilities <- unique(df1$toilet_facility.x)
 unique_home_ownership <- unique(df1$home_ownership.x)
 unique_house_type <- unique(df1$house_type.x)
 #unique_floor_resident <- unique(df1$floor_resident.x)
@@ -52,19 +68,19 @@ prefix_ceiling_presence <- "ceiling_presence_"
 
 
 # Create a new column for each unique water source
-for (source in unique_water_sources) {
+#for (source in unique_water_sources) {
   
-  col_name <- paste0(prefixdrinking, gsub("\\s+", "_", source))
-  df1[[col_name]] <- ifelse(df1$drinking_water_source.x == source, 1, 0)
+ # col_name <- paste0(prefixdrinking, gsub("\\s+", "_", source))
+ # df1[[col_name]] <- ifelse(df1$drinking_water_source.x == source, 1, 0)
 
-}
+#}
 
-for (source in unique_water_sources_tasks) {
+#for (source in unique_water_sources_tasks) {
   
-  col_name <- paste0(prefixtasks, gsub("\\s+", "_", source))
-  df1[[col_name]] <- ifelse(df1$basic_hh_tasks_water_source.x == source, 1, 0)
+ # col_name <- paste0(prefixtasks, gsub("\\s+", "_", source))
+#  df1[[col_name]] <- ifelse(df1$basic_hh_tasks_water_source.x == source, 1, 0)
   
-}
+#}
 
 for (source in unique_main_energy_sources) {
   
@@ -73,12 +89,12 @@ for (source in unique_main_energy_sources) {
   
 }
 
-for (source in unique_toilet_facilities) {
+#for (source in unique_toilet_facilities) {
   
-  col_name <- paste0(prefixtoilet, gsub("\\s+", "_", source))
-  df1[[col_name]] <- ifelse(df1$toilet_facility.x == source, 1, 0)
+#  col_name <- paste0(prefixtoilet, gsub("\\s+", "_", source))
+#  df1[[col_name]] <- ifelse(df1$toilet_facility.x == source, 1, 0)
   
-}
+#}
 
 
 for (source in unique_home_ownership) {
@@ -211,21 +227,12 @@ df1$shared_compound[df1$shared_compound.x == "Yes"] <- 1
 
 View(df1)
 
-
-
-
-
-  
-
 household_data <- df1 |>
-  select(271:350)
+  select(271:315)
 
 View(household_data)
 
-
-
-
-#Problematic columns Household Data
+#Check for Problematic columns in Household Data
 constant_columns <- sapply(household_data, function(x) length(unique(x))) == 1
 zero_variance_columns <- sapply(household_data, var) == 0
 problematic_columns <- colnames(household_data)[constant_columns | zero_variance_columns]
@@ -233,17 +240,9 @@ print(problematic_columns)
 
 
 household_data_detailed <- df1 |>
-  select(1,135,142,143,151,152,271:350) 
+  select(1,135,142,143,151,152,271:315) 
 
 View(household_data_detailed)
-
-#Problematic columns Household Data Detailed
-constant_columns_hh_data_detailed <- sapply(household_data_detailed, function(x) length(unique(x))) == 1
-zero_variance_columns_hh_data_detailed <- sapply(household_data_detailed, var) == 0
-problematic_columns_hh_data_detailed <- colnames(household_data_detailed)[constant_columns_hh_data_detailed | zero_variance_columns_hh_data_detailed]
-print(problematic_columns_hh_data_detailed)
-
-
 
 package_name <- "factoextra"
 
@@ -257,33 +256,68 @@ if (!require(package_name, character.only = TRUE)) {
   library(package_name, character.only = TRUE)
 }
 
-
-
-# Adapt this code for the PCA and you can also add a script on the  major task is to 
-# get the data in the right shape to feed to this pipeline. Your method or this one still requires us to 
-# code the data that we going to use 
-
-household_data1 <- data.frame(
-  # replace this data with the HH data keep the long, 
-  # lat, unique ID  ea_number new  etc 
-  TV = c(1, 0, 1, 1, 0),
-  Radio = c(1, 1, 0, 1, 0),
-  Water_Source = c(1, 0, 1, 1, 0),
-  Bathroom_Type = c(1, 0, 1, 1, 0),
-  Shared_Bathroom = c(0, 1, 0, 0, 1)
-)
-
-
-pca_result <- prcomp(household_data_detailed[,-c(1:5)], center = TRUE, scale. = TRUE)
+pca_result <- prcomp(household_data_detailed[,-c(1:6)], center = TRUE, scale. = TRUE)
 pc_scores <- pca_result$x 
 
 household_data_detailed$wealth_index <- pc_scores[,1]
 
+household_data_with_46_PCA_Scores <- household_data_detailed[,-52]
+
+
+# Define a prefix for the column names
+prefix <- "wealth_index_PCA_"
+
+# Loop through the first 46 columns of pc_scores and assign them to household_data_with_46_PCA_Scores
+for (i in 1:46) {
+  col_name <- paste0(prefix, i)  # Generate distinct column name
+  household_data_with_46_PCA_Scores[[col_name]] <- pc_scores[, i]  # Assign PCA scores to new column
+}
+  
+
 View(household_data_detailed)
+View(household_data_with_46_PCA_Scores)
+
+# Calculate mean and median
+mean_value <- mean(household_data_detailed$wealth_index)
+mode_value <- mode(household_data_detailed$wealth_index)
 
 ggplot(household_data_detailed,aes(x=settlement_type_new, y=wealth_index))+
-  geom_boxplot(data=household_data_detailed, aes(x=settlement_type_new, y=wealth_index), outlier.shape = NA) #+
- # geom_jitter(aes(color=settlement_type_new , size = wealth_index), width=0.08)
+ geom_boxplot(data=household_data_detailed, aes(x=settlement_type_new, y=wealth_index), outlier.shape = NA) + theme_manuscript()+
+#  geom_bar(data = household_data_detailed, aes(x=settlement_type_new, y=wealth_index, color=wealth_index), stat = "identity") + theme_manuscript()+
+geom_hline(yintercept = mean_value, color = "red", linetype = "dashed") +  # Add mean line
+  
+#geom_jitter(aes(color=settlement_type_new , size = wealth_index), width=0.08)+
+
+labs(title = "Plot of Wealth Index",
+  subtitle = "Plot showing settlement type by wealth index of households",
+   caption = "Data source : Cross Sectional Survey - Ibadan Wet Season Household Data"
+)
+
+
+
+
+library(openxlsx)
+
+# Create a new Excel workbook
+wb <- createWorkbook()
+
+# Add dataframes to different sheets
+
+addWorksheet(wb, sheetName = "Household Data")
+writeData(wb, sheet = "Household Data", x = df1)
+
+addWorksheet(wb, sheetName = "Household Data Detailed")
+writeData(wb, sheet = "Household Data Detailed" , x = household_data_detailed)
+
+addWorksheet(wb, sheetName = "PCA Results")
+writeData(wb, sheet = "PCA Results" , x = pca_result)
+
+addWorksheet(wb, sheetName = "Household PCA Scores")
+writeData(wb, sheet = "Household PCA Scores" , x = household_data_with_46_PCA_Scores)
+
+
+# Save the workbook to a file
+saveWorkbook(wb, file = "/Users/user/Urban Malaria Proj Dropbox/urban_malaria/projects/mathematical_model/spatial_analysis/Data for Wealth Index PCA/Data for Wealth Index Estimation Final Version 1.csv")
 
 
 
